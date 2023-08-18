1b2cb43d1ee6d47c966a542b8c646c07d826a467
Ramon Rodrigues <ramonrodsouza@gmail.com>
2023-08-18 20:25:20 -0300

    Adicionando Menu responsivo pt1

--- src/NavPrincipal/NavPrincipal.jsx
+++ src/NavPrincipal/NavPrincipal.jsx
@@ -1,95 +1,29 @@
-// import React, { useState } from "react";
-// import { ChakraProvider, Box, Flex, Menu, MenuButton, Button, MenuList, MenuItem, background } from "@chakra-ui/react";
-// import AbrirModal from "../AbrirModal/AbrirModal";
-// import { NavLink } from "react-router-dom";
-// import useGetVideo from "../Hooks/useGetVideo";
-// import { urlCategoria } from "../context/GetUrl";
-// import { IoIosMenu } from "react-icons/io";
-// import { IconContext } from "react-icons";
-
-// export function Navegacao () {
-//   const categoriasData = useGetVideo(urlCategoria);
-//   const { data: categorias, loading: loadingCategorias, error: errorCategorias } = categoriasData;
-//   const [showMenu, setShowMenu] = useState(false);
-
-//   if (loadingCategorias) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (errorCategorias) {
-//     return <div>Error fetching data.</div>;
-//   }
-
-//   const toggleMenu = () => {
-//     setShowMenu(!showMenu);
-//   };
-
-//   return (
-//     <Flex alignItems="center" bg='#fff'>
-//       {/* Renderiza o ícone de menu */}
-//       <IconContext.Provider value={{ size: "3rem" }}/>
-
-//       {/* Renderiza o menu com os links das categorias */}
-//       <Menu isLazy bg='#fff'>
-//         <MenuButton as={Button} ml="4">
-//           Categorias
-//         </MenuButton>
-//         <MenuList>
-//           {categorias.map((categoria) => (
-//             <MenuItem key={categoria.id} as={NavLink} to={`/${categoria.categoriaName}`}>
-//               {categoria.categoriaName}
-//             </MenuItem>
-//           ))}
-//         </MenuList>
-//       </Menu>
-
-//       {/* Renderiza o ícone de menu para abrir as opções em tela cheia */}
-//       {categorias.length > 4 && (
-//         <IconContext.Provider value={{ size: "2rem"}}>
-//           <IoIosMenu onClick={toggleMenu} style={{ cursor: "pointer", marginLeft: "auto"}} />
-//         </IconContext.Provider>
-//       )}
-
-//       <ChakraProvider>
-//         <AbrirModal />
-//       </ChakraProvider>
-//     </Flex>
-//   );
-// };
-
-
-
-
-
-
-
-
-
+import React, { useState } from "react";
 import styled from "styled-components";
-import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Flex, Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
 import AbrirModal from "../AbrirModal/AbrirModal";
 import { NavLink } from "react-router-dom";
 import useGetVideo from "../Hooks/useGetVideo";
 import { urlCategoria } from "../context/GetUrl";
+import { IoIosMenu } from "react-icons/io";
+import { IconContext } from "react-icons";
 
+export function Navegacao () {
+  const categoriasData = useGetVideo(urlCategoria);
+  const { data: categorias, loading: loadingCategorias, error: errorCategorias } = categoriasData;
+  const [showMenu, setShowMenu] = useState(false);
+
+  
 const UlPrincipal = styled.nav`
+  display: flex;
+  align-items: center;
+  text-shadow: 2px 3px 5px black; 
+  flex-grow: 1;
+  justify-content: space-around;
 
-    display: flex;
-    align-items: center;
-    text-shadow: 2px 3px 5px black; 
-    flex-grow:1;
-    justify-content: space-around;
-    
-
-    @media (min-width: 768px) {
-
-      font-size:1.5rem;
-    }
-
-`; 
-const LoadingMensagem = styled.span` 
-    
+  @media (min-width: 768px) {
+    font-size: 1.5rem;
+  }
 `;
 
 const styles = {
@@ -97,40 +31,116 @@ const styles = {
   textDecoration: "inherit",
   textShado: "2px 3px 5px shadowColor",
   textAlign: "center"
-  
 };
-export function Navegacao  () {
 
-  const categoriasData = useGetVideo(urlCategoria);
-  const { data: categorias, error: errorCategorias } = categoriasData;
+  if (loadingCategorias) {
+    return <div>Loading...</div>;
+  }
 
   if (errorCategorias) {
     return <div>Error fetching data.</div>;
   }
-  
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const categoriasExibicao = categorias.slice(0, 4);
+
+  if (categorias.length > 4) {
+    return (
+      <Flex alignItems="center" bg='#fff'>
+        {/* Renderiza o ícone de menu */}
+        <IconContext.Provider value={{ size: "3rem" }}/>
+
+        {/* Renderiza o menu com os links das categorias */}
+        <Menu isLazy bg='#fff'>
+          <MenuButton as={Button} ml="4">
+            Categorias
+          </MenuButton>
+          <MenuList>
+            {categoriasExibicao.map((categoria) => (
+              <MenuItem key={categoria.id} as={NavLink} to={`/${categoria.categoriaName}`}>
+                {categoria.categoriaName}
+              </MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
+        <ChakraProvider>
+          <AbrirModal />
+        </ChakraProvider>
+      </Flex>
+    );
+  }
+else {
   return (
-
-      // <UlPrincipal>
-      //   <NavLink style={styles} to="/desenho">Desenho</NavLink>
-      //   <NavLink style={styles} to="/filme">Filme</NavLink>
-      //   <NavLink style={styles} to="/musica">Musica</NavLink>
-      //   <ChakraProvider>
-      //     <AbrirModal/>
-      //   </ChakraProvider>
-
-      // </UlPrincipal> 
-  // )}
-      <UlPrincipal>
-      {/* Cria os links dinamicamente com base nos dados das categorias */}
-      {categorias.map((categoria) => (
+    <UlPrincipal>
+      {/* Cria os links dinamicamente com base nos dados das categorias filtradas */}
+      {categoriasExibicao.map((categoria) => (
         <NavLink key={categoria.id} style={styles} to={`/${categoria.categoriaName}`}>
           {categoria.categoriaName}
         </NavLink>
       ))}
       <ChakraProvider>
-      <AbrirModal/>
+        <AbrirModal />
       </ChakraProvider>
     </UlPrincipal>
   );
 }
-    
+
+}
+
+
+
+// import styled from "styled-components";
+// import React from "react";
+// import { ChakraProvider } from "@chakra-ui/react";
+// import AbrirModal from "../AbrirModal/AbrirModal";
+// import { NavLink } from "react-router-dom";
+// import useGetVideo from "../Hooks/useGetVideo";
+// import { urlCategoria } from "../context/GetUrl";
+
+// const UlPrincipal = styled.nav`
+//   display: flex;
+//   align-items: center;
+//   text-shadow: 2px 3px 5px black; 
+//   flex-grow: 1;
+//   justify-content: space-around;
+
+//   @media (min-width: 768px) {
+//     font-size: 1.5rem;
+//   }
+// `;
+
+// const styles = {
+//   color: 'white',
+//   textDecoration: "inherit",
+//   textShado: "2px 3px 5px shadowColor",
+//   textAlign: "center"
+// };
+
+// export function Navegacao() {
+//   const categoriasData = useGetVideo(urlCategoria);
+//   const { data: categorias, error: errorCategorias } = categoriasData;
+
+//   if (errorCategorias) {
+//     return <div>Error fetching data.</div>;
+//   }
+
+//   // Filtrar para exibir no máximo 4 categorias
+//   const categoriasExibicao = categorias.slice(0, 4);
+
+//   return (
+//     <UlPrincipal>
+//       {/* Cria os links dinamicamente com base nos dados das categorias filtradas */}
+//       {categoriasExibicao.map((categoria) => (
+//         <NavLink key={categoria.id} style={styles} to={`/${categoria.categoriaName}`}>
+//           {categoria.categoriaName}
+//         </NavLink>
+//       ))}
+//       <ChakraProvider>
+//         <AbrirModal />
+//       </ChakraProvider>
+//     </UlPrincipal>
+//   );
+// }
